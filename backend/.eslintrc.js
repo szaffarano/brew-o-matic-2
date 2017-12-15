9b8b88f0da35dd9059cfcb69644e77b72386625f
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2017-12-14 22:07:39 -0300

    Configure eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     'generator-star-spacing': 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     "space-before-function-paren": 0,
-    "no-unused-vars": 0
+    "no-unused-vars": 0,
+    "node/shebang": 0
   }
 }
