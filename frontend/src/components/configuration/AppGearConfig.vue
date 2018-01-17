4dafd884eb53d4ff881837da28c7f52ad32817f2
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2018-01-16 22:45:51 -0300

    Use dynamic-field to render the form

--- frontend/src/components/configuration/AppGearConfig.vue
+++ frontend/src/components/configuration/AppGearConfig.vue
@@ -9,27 +9,11 @@
           </v-flex>
             <v-flex xl3 lg3 md4 sm5 xs6 v-for="(v, k) in userSettingsMetadata" :key="k">
               <v-card class="pl-3 pr-3 pt-2">
-                <span v-if="v.type == 'Number'">
-                    <v-text-field
-                      :label="label(k)"
-                      v-model="userSettings.defaultValues[k]"
-                      :rules="rulesFor(label(k), v)"
-                    ></v-text-field>
-                </span>
-                <span v-if="v.type == 'String'">
-                  <v-text-field
-                    :label="label(k)"
-                    v-model="userSettings.defaultValues[k]"
-                    :rules="rulesFor(label(k), v)"
-                  ></v-text-field>
-                </span>
-                <span v-if="v.type == 'Boolean'">&nbsp;
-                  <v-switch
-                    :label="label(k)"
-                    v-model="userSettings.defaultValues[k]"
-                    :rules="rulesFor(label(k), v)"
-                  ></v-switch>
-                </span>
+                <dynamic-field
+                  :label="label(k)"
+                  :field="v"
+                  v-model="userSettings.defaultValues[k]">
+                </dynamic-field>
               </v-card>
             </v-flex>
         </v-layout>
@@ -48,41 +32,12 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import DynamicField from '../DynamicField'
 
 export default {
   data() {
     return {
       valid: false,
-      validatorFunctions: {
-        String: name => v => {
-          return true
-        },
-        Boolean: name => v => {
-          return true
-        },
-        Number: name => v => {
-          return (
-            !v ||
-            !Number.isNaN(Number.parseInt(v)) ||
-            !Number.isNaN(Number.parseFloat(v)) ||
-            this.$t('mongoose-Number', { path: name })
-          )
-        },
-        required: (name, validator) => v => {
-          return (
-            !!v ||
-            v === 0 ||
-            this.$t('mongoose-required', { path: name, ...validator })
-          )
-        },
-        min: (name, validator) => v => {
-          return (
-            !v ||
-            v >= validator.min ||
-            this.$t('mongoose-min', { path: name, ...validator })
-          )
-        }
-      }
     }
   },
   computed: {
@@ -96,19 +51,14 @@ export default {
     ]),
     label(name) {
       return this.$t(`settings.defaultValues.${name}`)
-    },
-    rulesFor(name, field) {
-      const validators = []
-      validators.push(this.validatorFunctions[field.type](name))
-      field.validators.forEach(v => {
-        validators.push(this.validatorFunctions[v.type](name, v))
-      })
-      return validators
     }
   },
   mounted() {
     this.getUserSettings()
     this.getUserSettingsMetadata()
+  },
+  components: {
+    DynamicField
   }
 }
 </script>
